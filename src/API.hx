980c2715cf1d06bd03b6dbf66061ff27d63d5d27
MSGhero <nj2cool4u@yahoo.com>
2014-07-15 02:47:12 -0500

    Connected to API
    Crossplat POST request., should trace a success JSON with a single medal
    inside the medals array.

--- src/API.hx
+++ src/API.hx
@@ -1,4 +1,6 @@
 package;
+import haxe.Http;
+import haxe.Log;
 
 /**
  * ...
@@ -6,11 +8,32 @@ package;
  */
 class API {
 
-	var apiId:String;
-	var encryptionKey:String;
+	var apiId:String = "37760:VcasLmE5"; // params for a test project I just made
+	var encryptionKey:String = "blnNtJNPbE9t2JAfxM0Dpx0yMxQL4rVW";
 	
 	public function new() {
 		
+		var h = new Http("http://www.ngads.com/gateway_v2.php");
+		h.onData = onRequest;
+		h.onError = showError;
+		h.onStatus = showStatus;
+		h.setHeader("Content-type", "application/x-www-form-urlencoded"); // no idea if this does anything
+		h.addParameter("tracker_id", apiId);
+		h.addParameter("command_id", "preloadSettings"); // not sure if other command_ids exist
+		h.request(true); // post
+	}
+	
+	function onRequest(s:String):Void {
+		Log.trace(Json.parse(s)); // I added a test medal, should come up under obj.medals
+		// connected!
+	}
+	
+	function showError(s:String):Void {
+		Log.trace(s);
+	}
+	
+	function showStatus(i:Int):Void {
+		Log.trace(i);
 	}
 	
 }
