ca2df6ade37d194844f3bd53df14f62328c07656
MSGhero <nj2cool4u@yahoo.com>
2014-07-16 20:33:13 -0500

    Added encryption process
    Messy, will clean up later. ArcFour comes from "format" haxelib.
    
    Need to spead this code around into other classes.

--- src/API.hx
+++ src/API.hx
@@ -1,6 +1,8 @@
 package;
 import flash.Lib;
+import haxe.crypto.Md5;
 import haxe.Http;
+import haxe.io.Bytes;
 import haxe.Json;
 import haxe.Log;
 
@@ -16,6 +18,8 @@ class API {
 	var username:String;
 	var userId:UInt;
 	var sessionId:String;
+	
+	var radix79:String = "/g8236klvBQ#&|;Zb*7CEA59%s`Oue1wziFp$rDVY@TKxUPWytSaGHJ>dmoMR^<0~4qNLhc(I+fjn)X";
 	
 	public function new() {
 		
@@ -73,7 +77,6 @@ class API {
 		// "n save group initialized."
 		
 		log("Connection complete!");
-		
 	}
 	
 	function showError(s:String):Void {
@@ -84,6 +87,71 @@ class API {
 	
 	function showStatus(i:Int):Void {
 		// Log.trace(i);
+	}
+	
+	function sendEncrypted(unsecure:Dynamic, seedLen:Int = 16, ?requestCallback:String->Void):Void {
+		
+		// unsecure is has everything needed except for seed and sessionid
+		
+		// messy
+		var seed = getKey(seedLen);
+		unsecure.session_id = sessionId;
+		var secure = Json.stringify(unsecure);
+		
+		var secureRC4 = Bytes.alloc(secure.length);
+		var secureBytes = Bytes.ofString(secure);
+		
+		var rc4 = new ArcFour(Bytes.ofString(encryptionKey));
+		rc4.run(secureBytes, 0, secureBytes.length, secureRC4, 0);
+		
+		var encrypted = Md5.encode(seed) + secureRC4.toHex();
+		
+		var sixhex:String, decimal:Int, four:String;
+		var compressed = "";
+		for (i in 0...Math.ceil(encrypted.length / 6)) {
+			
+			sixhex = '0x${encrypted.substr(i * 6, 6)}';
+			decimal = Std.parseInt(sixhex);
+			four = toBase79(decimal);
+			
+			while (four.length < 4) four = '${radix79.charAt(0)}${four}'; // there's a bug here if the last iteration grabs less than 6 chars, will look into it
+			compressed += four;
+		}
+		
+		compressed = '${encrypted.length % 6}${compressed}';
+		
+		var h = new Http("http://www.ngads.com/gateway_v2.php");
+		h.onData = requestCallback;
+		h.onError = showError;
+		h.onStatus = showStatus;
+		h.setHeader("Content-type", "application/x-www-form-urlencoded"); // no idea if this does anything
+		h.addParameter("command_id", "securePacket");
+		h.addParameter("tracker_id", apiId);
+		h.addParameter("seed", seed);
+		h.addParameter("secure", compressed);
+		
+		h.request(true);
+	}
+	
+	function getKey(length:Int):String {
+		var key = "";
+		for (i in 0...length) {
+			key += radix79.charAt(Std.int(Math.random() * 79));
+		}
+		return key;
+	}
+	
+	function toBase79(dec:Int):String {
+		
+		var res = "";
+		var r:Int;
+		while (dec > 0) {
+			r = dec % 79;
+			res = '${radix79.charAt(r)}${res}';
+			dec = Std.int(dec / 79);
+		}
+		
+		return res;
 	}
 	
 	inline function log(s:String):Void {
